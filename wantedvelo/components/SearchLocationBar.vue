3d180a6516e447be2fd9506de3c2b863394b2da1
Priandey <riandeypierre@gmail.com>
2021-03-31 19:32:01 +0200

    Add SearchLocationBar component
    Allowing locating search through government API.

---
+++ wantedvelo/components/SearchLocationBar.vue
@@ -0,0 +1,91 @@
+<template>
+    <!-- Todo: The component -->
+  <v-card
+    color="secondary"
+    dark
+  >
+    <v-card-text>
+      <v-autocomplete
+        v-model="model"
+        :items="items"
+        :loading="isLoading"
+        :search-input.sync="search"
+        color="white"
+        hide-no-data
+        hide-selected
+        :filter="customFilter"
+        item-text="cityname"
+        item-value="cityname"
+        label="Chercher une commune"
+        placeholder="Essayez un nom de commune, ou un code postal"
+        prepend-icon="mdi-magnify"
+        return-object
+      ></v-autocomplete>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+    export default {
+        name: "SearchLocationBar",
+      data: () => ({
+        entries: [],
+        isLoading: false,
+        model: null,
+        search: null,
+      }),
+      computed: {
+          items() {
+            return this.entries
+          }
+      },
+      watch: {
+        search (val) {
+          // Items have already been requested
+          if (this.isLoading) return
+
+          this.isLoading = true;
+          this.entries = [];
+
+          this.$axios.get("https://api-adresse.data.gouv.fr/search/", {
+            params: {
+              q:val,
+              type: "municipality",
+              autocomplete:"1",
+              limit:"5"
+            }
+          })
+            .then(response => response.data.features)
+            .then(response => {
+              response.forEach(item => {
+                this.entries.push({
+                  cityname: item.properties.city + ', ' + item.properties.postcode,
+                  coordinates: item.geometry.coordinates
+                })
+              })
+            })
+            .catch(err => {
+              console.log(err)
+            })
+            .finally(() => (this.isLoading = false))
+        },
+        model: function(self) {
+            this.$store.commit('setPoint', {
+              lat: self.coordinates[1],
+              lon: self.coordinates[0],
+            });
+        }
+      },
+      methods: {
+          customFilter(item, queryText, itemText) {
+            const cityname = item.cityname.toLowerCase();
+            const searchText = queryText.toLowerCase();
+
+            return cityname.search(searchText) > -1
+          }
+      }
+    }
+</script>
+
+<style scoped>
+</style>
--- wantedvelo/pages/near.vue
+++ wantedvelo/pages/near.vue
@@ -1,17 +1,20 @@
 <template>
     <v-main>
-      <LocateUser />
-      <v-btn @click="centerElsewhere"></v-btn>
-      <div id="map-wrap" style="height: 82vh">
-        <client-only>
+      <v-card>
+        <SearchLocationBar></SearchLocationBar>
+        <LocateUser></LocateUser>
+        <v-btn @click="centerElsewhere"></v-btn>
+      </v-card>
+      <v-img> <!-- Map is warped in an image to prevent clashes between vuetify z-index (0-10) and leaflet z-index(100-1100 -->
+      <div id="map-wrap" style="height: 100vh">
           <l-map :zoom=9 :center="centerPoint">
             <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
             <l-marker :lat-lng="centerPoint">
               <l-tooltip><v-card width="150"><v-card-title>COUCOU</v-card-title></v-card></l-tooltip>
             </l-marker>
           </l-map>
-        </client-only>
       </div>
+      </v-img>
     </v-main>
 </template>
 
