fd072b2d287ce32941b1ea17e101f25f4991940c
Priandey <riandeypierre@gmail.com>
2021-05-21 14:21:13 +0200

    Switched bike fetching to server side

--- components/BikeAlert.vue
+++ components/BikeAlert.vue
@@ -51,7 +51,7 @@
 <script>
     export default {
       name: "BikeAlert",
-      props: ['bikeId', 'inModal'],
+      props: ['bikeId', 'inModal', 'providedBike'],
       data() {
         return {
           bike: {
@@ -70,7 +70,11 @@
           .catch(e => console.log(e))
       },
       mounted() {
-        this.$fetch();
+        if(this.providedBike) {
+          this.bike = this.providedBike;
+        } else {
+          this.$fetch();
+        }
       },
       computed:{
         constructedURL() {
@@ -101,63 +105,6 @@
           }
         },
       },
-
-      head() {
-        return {
-          meta: [
-            {
-              hid: 'description',
-              name: 'description',
-              content: 'Le vélo identifié ' + this.bike.reference + "a disparu, aidez son propriétaire a le retrouver"
-            },
-            {
-              hid: 'twitter:title',
-              name: 'twitter:title',
-              content: this.bike.reference + ', ce vélo a disparu !'
-            },
-            {
-              hid: 'twitter:description',
-              name: 'twitter:description',
-              content: 'Le vélo identifié ' + this.bike.reference + "a disparu, aidez son propriétaire a le retrouver"
-            },
-            {
-              hid: 'twitter:image',
-              name: 'twitter:image',
-              content: this.bike.picture
-            },
-            {
-              hid: 'twitter:image:alt',
-              name: 'twitter:image:alt',
-              content: "Image du vélo " + this.bike.reference
-            },
-            {
-              hid: 'og:title',
-              property: 'og:title',
-              content: this.bike.reference + ', ce vélo a disparu !',
-            },
-            {
-              hid: 'og:description',
-              property: 'og:description',
-              content: 'Le vélo identifié ' + this.bike.reference + "a disparu, aidez son propriétaire a le retrouver"
-            },
-            {
-              hid: 'og:image',
-              property: 'og:image',
-              content: this.bike.picture
-            },
-            {
-              hid: 'og:image:secure_url',
-              property: 'og:image:secure_url',
-              content: this.bike.picture
-            },
-            {
-              hid: 'og:image:alt',
-              property: 'og:image:alt',
-              content: "Image du vélo " + this.bike.reference
-            }
-          ]
-        }
-      }
     }
 </script>
 
--- pages/alert/_id.vue
+++ pages/alert/_id.vue
@@ -1,6 +1,7 @@
 <template>
   <bike-alert
-  :bike-id="id"></bike-alert>
+  :bike-id="id"
+  :provided-bike="bike"></bike-alert>
 </template>
 
 <script>
@@ -12,6 +13,68 @@
             id : this.$route.params.id
           }
       },
+
+      async asyncData(context) {
+        let bike = await context.app.$axios.$get('/bike/'+context.params.id+'/');
+        return { bike }
+      },
+
+      head() {
+        return {
+          meta: [
+            {
+              hid: 'description',
+              name: 'description',
+              content: 'Le vélo identifié ' + this.bike.reference + "a disparu, aidez son propriétaire a le retrouver"
+            },
+            {
+              hid: 'twitter:title',
+              name: 'twitter:title',
+              content: this.bike.reference + ', ce vélo a disparu !'
+            },
+            {
+              hid: 'twitter:description',
+              name: 'twitter:description',
+              content: 'Le vélo identifié ' + this.bike.reference + "a disparu, aidez son propriétaire a le retrouver"
+            },
+            {
+              hid: 'twitter:image',
+              name: 'twitter:image',
+              content: this.bike.picture
+            },
+            {
+              hid: 'twitter:image:alt',
+              name: 'twitter:image:alt',
+              content: "Image du vélo " + this.bike.reference
+            },
+            {
+              hid: 'og:title',
+              property: 'og:title',
+              content: this.bike.reference + ', ce vélo a disparu !',
+            },
+            {
+              hid: 'og:description',
+              property: 'og:description',
+              content: 'Le vélo identifié ' + this.bike.reference + "a disparu, aidez son propriétaire a le retrouver"
+            },
+            {
+              hid: 'og:image',
+              property: 'og:image',
+              content: this.bike.picture
+            },
+            {
+              hid: 'og:image:secure_url',
+              property: 'og:image:secure_url',
+              content: this.bike.picture
+            },
+            {
+              hid: 'og:image:alt',
+              property: 'og:image:alt',
+              content: "Image du vélo " + this.bike.reference
+            }
+          ]
+        }
+      }
 
     }
 </script>
