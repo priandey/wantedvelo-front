8babb43d4cccb83026bf9ae975eaf94603f0f233
Pierre Riandey <42967943+priandey@users.noreply.github.com>
2021-09-25 16:24:22 +0200

    added manifeste and cgu

---
+++ pages/manifeste.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-card>
+    <v-containe>
+      <v-row>
+        <v-col
+          cols="12"
+          lg="6"
+        >
+          <v-card-title>Le Manifeste de Wanted Vélo</v-card-title>
+          <v-card-subtitle>Cyclistes déclarez le vol de votre vélo</v-card-subtitle>
+          <v-card-text>
+            <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/610205468?h=3eaf5df637&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+          </v-card-text>
+
+          <v-card-title>Le vol des vélos est un frein à l’usage du vélo</v-card-title>
+
+          <v-card-text>Wanted Vélo est convaincu que le vol des vélos est l’un des freins majeurs au développement de l’usage du vélo en France.
+            Militants de la cause du vélo, les membres de Wanted Vélo veulent agir pour réduire ce phénomène de vol actuellement sous-estimé.</v-card-text>
+
+          <v-card-title>Faciliter la déclaration de vol est nécessaire</v-card-title>
+
+          <v-card-text>Les procédures en place ne permettent pas une prise en charge efficace des victimes de vol de vélos.
+          Pris unitairement un vol de vélo déclaré à la police nationale est souvent considéré comme anodin et ne donne pas ou rarement de suite.
+            Wanted Vélo veut rendre visible l’invisible et permettre à chacun de facilement déclarer la disparition de sa petite reine.</v-card-text>
+
+          <v-card-title>Offrir un premier soutien aux victimes de vol est utile</v-card-title>
+
+          <v-card-text>Dès la découverte de la disparition de son vélo, le cycliste est désemparé et se sent impuissant.
+            Wanted Vélo veut offrir aux victimes un moyen d’alerte immédiat et une possibilité d’être contacté si le vélo est retrouvé.</v-card-text>
+
+          <v-card-title>roduire des statistiques exhaustives sur les vols de vélo est bénéfique</v-card-title>
+
+          <v-card-text>En permettant aux autorités locales et nationales et aux associations de vélos de disposer de données de localisation, de catégorisation et de circonstances des vols de vélo, Wanted Vélo permet d’améliorer la connaissance du phénomène.
+            Celle-ci est nécessaire pour appuyer toutes les démarches visant à agir contre le vol des vélos (investigations policières, aménagements, prévention,…).</v-card-text>
+
+          <v-card-title>Construire un service utile avec les acteurs concernés est notre objectif</v-card-title>
+
+          <v-card-text>Wanted Vélo propose de construire une plateforme avec l’ensemble des parties prenantes. Wanted Vélo a pris la forme d’une association loi 1901 pour permettre aux structures militantes du vélo d’intégrer la gouvernance de la maintenance et de l’évolution de celle-ci.
+            Wanted Vélo souhaite nouer des relations de partenariat avec les acteurs publics aux différentes mailles territoriales sous la forme de conventions.</v-card-text>
+
+          <v-card-title>Garantir un service sans publicité ni vente des données personnelles est notre engagement</v-card-title>
+
+          <v-card-text>Wanted Vélo finance le projet par le biais des cotisations des structures adhérentes, par les dons des utilisateurs de la plateforme, par les conventions passées avec les collectivités et par les subventions publiques.
+          Aucune publicité ni vente de données ne sera mise en place.
+            Wanted Vélo s'engage à ne pas tirer profit de ce service dans un but d’enrichissement personnel et à maintenir l’esprit militant de la démarche.</v-card-text>
+
+          <v-card-text>Colombes le 31 août 2021<br />
+            Le bureau élargi fondateur</v-card-text>
+
+          <v-card-text>
+            Nichola Soudière, Président – Médiateur culturel à la cave à Théatre de Colombes<br />
+            Nadine Lahoud, Trésorière – Déléguée Générale de l’association Veni Verdi<br/>
+            Joël Riandey, Secrétaire – Responsable Informatique chez EDF<br/>
+            Thomas Lalleman, Responsable de la commission technique – Concepteur développeur chez<br />
+            Michel Desportes, Responsable de la commission Relations institutionnelles<br />
+          </v-card-text>
+        </v-col>
+      </v-row>
+    </v-containe>
+  </v-card>
+
+</template>
+
+<script>
+    export default {
+        name: "charte"
+    }
+</script>
+
+<style scoped>
+
+</style>
